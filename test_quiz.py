5a2086e6dc0acada5fbbbdb1c85dfc2f2d4e8aee
Prajith-2024 <iamironman830@gmail.com>
2026-04-29 18:00:56 +0530

    Fixed Chrome headless issue

--- test_quiz.py
+++ test_quiz.py
@@ -2,15 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 def test_quiz():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")   # 🔥 FIX
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
 
     driver.get("http://localhost:5000")
 
+    time.sleep(3)
+
     driver.find_element(By.XPATH, "//input[@value='4']").click()
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
+
+    time.sleep(2)
 
     assert "Score: 1" in driver.page_source
 
